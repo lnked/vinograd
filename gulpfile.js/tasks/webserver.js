dca215e65dca373ec02739ae309c751e248f7ad5
ed <ed.proff@gmail.com>
2017-05-17 17:05:01 +0300

    iupda

--- gulpfile.js/tasks/webserver.js
+++ gulpfile.js/tasks/webserver.js
@@ -3,11 +3,14 @@
 const browserSync = require('browser-sync').create();
 
 module.exports = function(config) {
-	config = config || { server: {} };
+    config = config || { server: {} };
 
-	return function() {
-		browserSync.init(config.server);
+    return function(callback) {
+
+        browserSync.init(config.server);
         browserSync.watch(config.app + '**/*.*').on('change', browserSync.reload);
-	};
+
+        callback();
+    };
 
 };
