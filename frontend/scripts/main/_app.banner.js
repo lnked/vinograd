1658fae20f1864694dd69709e8afdd4854a5185c
Celebro dev <ed.proff@gmail.com>
2018-07-11 01:46:07 +0300

    update

--- frontend/scripts/main/_app.banner.js
+++ frontend/scripts/main/_app.banner.js
@@ -1,72 +1,72 @@
-let app = app || {};
+// let app = app || {};
 
-((body => {
-    "use strict";
+// ((body => {
+//     "use strict";
 
-    const banner_id = 'banner-last-rooms';
+//     const banner_id = 'banner-last-rooms';
 
-    app.banner = {
+//     app.banner = {
 
-        initClose: function() {
-            $('.banner.show').click(function(){
-                if ($(`#${banner_id}`).length) {
-                    var $banner = $(`#${banner_id}`);
+//         initClose: function() {
+//             $('.banner.show').click(function(){
+//                 if ($(`#${banner_id}`).length) {
+//                     var $banner = $(`#${banner_id}`);
 
-                    if ($banner.hasClass('show')) {
-                        $banner.removeClass('animate');
+//                     if ($banner.hasClass('show')) {
+//                         $banner.removeClass('animate');
 
-                        setTimeout(function(){
-                            $banner.removeClass('show');
-                        }, 300);
-                    }
-                }
-            });
-        },
+//                         setTimeout(function(){
+//                             $banner.removeClass('show');
+//                         }, 300);
+//                     }
+//                 }
+//             });
+//         },
 
-        make: function(delay)
-        {
-            const _this = this;
+//         make: function(delay)
+//         {
+//             const _this = this;
 
-            const close = function ($b) {
-                $b.removeClass('animate');
+//             const close = function ($b) {
+//                 $b.removeClass('animate');
 
-                setTimeout(() => {
-                    $b.removeClass('show');
-                }, 400);
-            }
+//                 setTimeout(() => {
+//                     $b.removeClass('show');
+//                 }, 400);
+//             }
 
-            if ($(`#${banner_id}`).length) {
-                const $banner = $(`#${banner_id}`);
+//             if ($(`#${banner_id}`).length) {
+//                 const $banner = $(`#${banner_id}`);
 
-                $(document).on('keyup', function(e) {
-                    if (e.keyCode == 27) {
-                        if ($banner.hasClass('show')) {
-                            close($banner);
-                        }
-                    }
-                });
+//                 $(document).on('keyup', function(e) {
+//                     if (e.keyCode == 27) {
+//                         if ($banner.hasClass('show')) {
+//                             close($banner);
+//                         }
+//                     }
+//                 });
 
-                $banner.on('click', '.banner__image__close', function(e) {
-                    e.preventDefault();
-                    close($banner);
-                });
+//                 $banner.on('click', '.banner__image__close', function(e) {
+//                     e.preventDefault();
+//                     close($banner);
+//                 });
 
-                setTimeout(function(){
-                    $banner.addClass('show');
+//                 setTimeout(function(){
+//                     $banner.addClass('show');
 
-                    setTimeout(function(){
-                        $banner.addClass('animate');
-                    }, 10);
+//                     setTimeout(function(){
+//                         $banner.addClass('animate');
+//                     }, 10);
 
-                    _this.initClose();
-                }, delay * 1000);
-            }
-        },
+//                     _this.initClose();
+//                 }, delay * 1000);
+//             }
+//         },
 
-        init: function() {
-            this.make(2);
-        }
+//         init: function() {
+//             this.make(2);
+//         }
 
-    };
+//     };
 
-}))(document.body);
+// }))(document.body);
